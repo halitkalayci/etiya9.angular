f7f72fabba5924de6b56318c53170e4342fdd66d
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-10-15 10:23:22 +0300

    lifecycle of a comp

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html', // direkt html'i ts içerisinde yazabiliriz.
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit, OnChanges {
+  // değişkenler, ctor, lifecycle methods, custom methods
+  count: number = 10;
+
+  constructor() {}
+  // component lifecycle methods
+  ngOnInit(): void {
+    console.log('Merhaba 2');
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges çalıştı', changes);
+  }
+  // component lifecycle methods
+
+  increaseCount() {
+    this.count++;
+  }
+
+  decreaseCount() {
+    this.count--;
+  }
+}
